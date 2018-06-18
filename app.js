eae223f6422bf8492f0f9b13bbb169da5b96d89a
marceloprof <eck.marcelo@gmail.com>
2018-06-18 01:00:45 -0300

    first class - install node, npm, express and using ejs lib

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine','ejs');
+
+app.get('/products', function (req, res) {
+    res.render("products/list")
+});
+
+app.listen(3000, function () {
+    console.log("server is runnning");
+});
